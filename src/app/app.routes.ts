2373b23f8c7a71b46f6f4c562ad17aa8fe2f3c2a
EduardoAH24 <1003620231@unajma.edu.pe>
2025-11-29 21:51:24 -0500

    Configurando rutas principales de la aplicación

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
+import { ClassList } from './pages/class-list/class-list';
+import { ClassForm } from './pages/class-form/class-form';
+import { Statistics } from './pages/statistics/statistics';
+import { Calendar } from './pages/calendar/calendar';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: Login },
+  { path: 'register', component: Register },
+  { path: 'clases', component: ClassList },
+  { path: 'clases/nueva', component: ClassForm },
+  { path: 'estadisticas', component: Statistics },
+  { path: 'calendario', component: Calendar },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '**', redirectTo: 'login' }
+];
